d50ad586b8a469c5d548cecefb6d2a28f0a19ac2
GreyRuler <daniil_vasilenko757@mail.ru>
2022-02-08 09:26:59 +0300

    Доработка

--- Music lover/src/main/kotlin/main.kt
+++ Music lover/src/main/kotlin/main.kt
@@ -2,6 +2,6 @@ fun main() {
     val totalPrice = 1_000
     val discount = if (totalPrice in 1001..10000) 100 else if (totalPrice >= 10001) totalPrice * 0.05 else 0
     val musicLover = true
-    val customerDiscount = (totalPrice.toDouble() - discount.toDouble()) * 0.01
-    val totalDiscount = if (musicLover) (discount.toDouble()+customerDiscount) else discount
+    val totalDiscount = if (musicLover) (discount.toInt()+(totalPrice - discount.toInt()) * 0.01).toInt() else discount
+    println(totalDiscount)
 }
--- People-People/src/main/kotlin/main.kt
+++ People-People/src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main() {
     for (likes: Int in 1..30) {
-        if (likes == 1 || likes % 10 == 1 && likes % 100 != 11) {
+        if (likes % 10 == 1 && likes % 100 != 11) {
             println("Понравилось $likes человеку")
         } else {
             println("Понравилось $likes людям")
